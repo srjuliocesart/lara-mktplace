8ac6807d3cb42275389870d39148bdedc2627081
srjuliocesart <srjuliocesart@gmail.com>
2023-08-07 13:44:39 -0300

    removing unnecessary column

--- database/migrations/2023_07_24_202622_create_user_order_table.php
+++ database/migrations/2023_07_24_202622_create_user_order_table.php
@@ -14,14 +14,12 @@ return new class extends Migration
         Schema::create('user_orders', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('user_id');
-            $table->unsignedBigInteger('store_id');
             $table->string('reference');
             $table->string('pagseguro_code');
             $table->integer('pagseguro_status');
             $table->text('items');
             $table->timestamps();
             $table->foreign('user_id')->references('id')->on('users');
-            $table->foreign('store_id')->references('id')->on('stores');
         });
     }
 
